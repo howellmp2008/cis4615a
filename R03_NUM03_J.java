653d2c12bab2a21279acc4e42121de0f4b793b9f
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 14:53:21 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -2,3 +2,5 @@
 public static int getInteger(DataInputStream is) throws IOException {
   return is.readInt(); 
 }
+
+Link: https://wiki.sei.cmu.edu/confluence/display/java/NUM03-J.+Use+integer+types+that+can+fully+represent+the+possible+range+of++unsigned+data

e4ea4c07d6e16ceb8f784de13290c439e2db2b3d
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:07:14 -0400

    Create R15_SEC01_J.java

---
+++ R15_SEC01_J.java
@@ -0,0 +1,26 @@
+
+private void privilegedMethod(final String filename)
+                              throws FileNotFoundException {
+  final String cleanFilename;
+  try {
+    cleanFilename = cleanAFilenameAndPath(filename);
+  } catch (/* exception as per spec of cleanAFileNameAndPath */) {
+    // Log or forward to handler as appropriate based on specification
+    // of cleanAFilenameAndPath
+  }
+  try {
+    FileInputStream fis =
+        (FileInputStream) AccessController.doPrivileged(
+          new PrivilegedExceptionAction() {
+        public FileInputStream run() throws FileNotFoundException {
+          return new FileInputStream(cleanFilename);
+        }
+      }
+    );
+    // Do something with the file and then close it
+  } catch (PrivilegedActionException e) {
+    // Forward to handler
+  }
+}
+
+Link: https://wiki.sei.cmu.edu/confluence/display/java/SEC01-J.+Do+not+allow+tainted+variables+in+privileged+blocks

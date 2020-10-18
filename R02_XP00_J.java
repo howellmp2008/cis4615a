bcb1da01c6f1b3f9d46541c060a1c47d0bcbe569
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:20:48 -0400

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,9 +1,21 @@
-
+//Non-compliant code
 public void deleteFile(){
  
   File someFile = new File("someFileName.txt");
   // Do something with someFile
   someFile.delete();
+ 
+}
+
+//Compliant code
+
+public void deleteFile(){
+ 
+  File someFile = new File("someFileName.txt");
+  // Do something with someFile
+  if (!someFile.delete()) {
+    // Handle failure to delete the file
+  }
  
 }
 

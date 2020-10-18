5ecae4ddb058fe183f2ecebb484dfaad272a011b
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:19:53 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,7 +1,20 @@
+//Non-compliant code
 if (loginSuccessful) {
   logger.severe("User login succeeded for: " + username);
 } else {
   logger.severe("User login failed for: " + username);
+}
+
+//Compliant code
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+  logger.severe("User login failed for: " + sanitizeUser(username));
+}
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
 }
 
 Link:  https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input

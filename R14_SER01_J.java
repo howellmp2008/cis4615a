2b00515114838ef9636580e3c47c5ba419b1ca3d
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:27:36 -0400

    Update R14_SER01_J.java

--- R14_SER01_J.java
+++ R14_SER01_J.java
@@ -1,4 +1,4 @@
-
+//Non-compliant Code
 public class Ser implements Serializable {
   private final long serialVersionUID = 123456789;
   private Ser() {
@@ -12,6 +12,17 @@ public class Ser implements Serializable {
       throws IOException, ClassNotFoundException {
     stream.defaultReadObject();
   }
+}
+
+//Compliant Code
+private void writeObject(final ObjectOutputStream stream)
+    throws IOException {
+  stream.defaultWriteObject();
+}
+ 
+private void readObject(final ObjectInputStream stream)
+    throws IOException, ClassNotFoundException {
+  stream.defaultReadObject();
 }
 
 Link: https://wiki.sei.cmu.edu/confluence/display/java/SER01-J.+Do+not+deviate+from+the+proper+signatures+of+serialization+methods

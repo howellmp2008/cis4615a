f150f1b5b6ba1610be06927d0a09f5f5436a0fe8
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:23:11 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,7 +1,15 @@
+//Non-compliant Code
 BigInteger x = new BigInteger("530500452766");
 byte[] byteArray = x.toByteArray();
 String s = new String(byteArray);
 byteArray = s.getBytes();
 x = new BigInteger(byteArray);
+
+//Compliant Code
+BigInteger x = new BigInteger("530500452766");
+String s = x.toString();  // Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray); 
+x = new BigInteger(ns);
 
 Link: https://wiki.sei.cmu.edu/confluence/display/java/STR03-J.+Do+not+encode+noncharacter+data+as+a+string

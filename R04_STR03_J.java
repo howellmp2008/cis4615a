bd1b7f86fd1461535870595c927ae172b06f3603
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:37:23 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,3 +1,5 @@
+//Rule 04. Characters and Strings (STR)
+//STR03-J. Do not encode noncharacter data as a string
 //Non-compliant Code
 BigInteger x = new BigInteger("530500452766");
 byte[] byteArray = x.toByteArray();

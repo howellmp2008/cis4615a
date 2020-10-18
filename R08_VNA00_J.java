3b79db6d3b8549e5cc3bd553670910ccc30e5c7b
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:00:19 -0400

    Create R08_VNA00_J.java

---
+++ R08_VNA00_J.java
@@ -0,0 +1,21 @@
+
+final class ControlledStop implements Runnable {
+  private volatile boolean done = false;
+  
+  @Override public void run() {
+    while (!done) {
+      try {
+        // ...
+        Thread.currentThread().sleep(1000); // Do something
+      } catch(InterruptedException ie) {
+        Thread.currentThread().interrupt(); // Reset interrupted status
+      }
+    }   
+  }
+ 
+  public void shutdown() {
+    done = true;
+  }
+}
+
+Link: https://wiki.sei.cmu.edu/confluence/display/java/VNA00-J.+Ensure+visibility+when+accessing+shared+primitive+variables

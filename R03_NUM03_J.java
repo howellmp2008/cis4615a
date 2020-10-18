8b5f656ead3660b1878a5815254690ffe52c1c21
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:21:54 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,6 +1,11 @@
-
+//Non-compliant code
 public static int getInteger(DataInputStream is) throws IOException {
   return is.readInt(); 
+}
+
+//Compliant code
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
 
 Link: https://wiki.sei.cmu.edu/confluence/display/java/NUM03-J.+Use+integer+types+that+can+fully+represent+the+possible+range+of++unsigned+data

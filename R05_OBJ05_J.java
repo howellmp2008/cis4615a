3694938a7b4af529f6c314aebf679ea41811f0ea
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 14:48:07 -0400

    Create R05_OBJ05_J.java

---
+++ R05_OBJ05_J.java
@@ -0,0 +1,21 @@
+
+class MutableClass {
+  private Date[] date;
+ 
+  public MutableClass() {
+    date = new Date[20];
+    for(int i = 0; i < date.length; i++) {
+      date[i] = new Date();
+    }
+  }
+ 
+  public Date[] getDate() {
+    Date[] dates = new Date[date.length];
+    for (int i = 0; i < date.length; i++) {
+      dates[i] = (Date) date[i].clone();
+    }
+    return dates;
+  }
+}
+
+Link: https://wiki.sei.cmu.edu/confluence/display/java/OBJ05-J.+Do+not+return+references+to+private+mutable+class+members

d1f6564502ced4878a7f30b2dbe5324b11eda116
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:02:09 -0400

    Create R13_FIO08_J.java

---
+++ R13_FIO08_J.java
@@ -0,0 +1,9 @@
+
+FileInputStream in;
+// Initialize stream
+byte data;
+while ((data = (byte) in.read()) != -1) {
+  // ...
+}
+
+Link: https://wiki.sei.cmu.edu/confluence/display/java/FIO08-J.+Distinguish+between+characters+or+bytes+read+from+a+stream+and+-1

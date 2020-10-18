946bfdebc6914091f76b15c1895994053cb9b12a
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 14:57:11 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -4,3 +4,5 @@ String s = x.toString();  // Valid character data
 byte[] byteArray = s.getBytes();
 String ns = new String(byteArray); 
 x = new BigInteger(ns);
+
+https://wiki.sei.cmu.edu/confluence/display/java/STR03-J.+Do+not+encode+noncharacter+data+as+a+string

8cb06e37f76b99649145deb92f21054d80e50a27
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:26:42 -0400

    Update R13_FIO08_J.java

--- R13_FIO08_J.java
+++ R13_FIO08_J.java
@@ -1,9 +1,19 @@
-
+//Non-compliant Code
 FileInputStream in;
 // Initialize stream
 byte data;
 while ((data = (byte) in.read()) != -1) {
   // ...
+}
+
+//Compliant Code
+FileInputStream in;
+// Initialize stream
+int inbuff;
+byte data;
+while ((inbuff = in.read()) != -1) {
+  data = (byte) inbuff;
+  // ... 
 }
 
 Link: https://wiki.sei.cmu.edu/confluence/display/java/FIO08-J.+Distinguish+between+characters+or+bytes+read+from+a+stream+and+-1

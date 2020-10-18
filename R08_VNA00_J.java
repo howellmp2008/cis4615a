4c0bad8fe8f5d4b84cd0ecbdca226f6c58ce7a82
howellmp2008 <46573044+howellmp2008@users.noreply.github.com>
2020-10-18 15:25:50 -0400

    Update R08_VNA00_J.java

--- R08_VNA00_J.java
+++ R08_VNA00_J.java
@@ -1,6 +1,26 @@
-
+//Non-compliant Code
 final class ControlledStop implements Runnable {
   private boolean done = false;
+  
+  @Override public void run() {
+    while (!done) {
+      try {
+        // ...
+        Thread.currentThread().sleep(1000); // Do something
+      } catch(InterruptedException ie) {
+        Thread.currentThread().interrupt(); // Reset interrupted status
+      }
+    }   
+  }
+ 
+  public void shutdown() {
+    done = true;
+  }
+}
+
+//Compliant Code
+final class ControlledStop implements Runnable {
+  private volatile boolean done = false;
   
   @Override public void run() {
     while (!done) {
